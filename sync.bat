89c7bfff7875964ca705b67c4c3f9d251be8a266
Christian Teichert <c.teichert@citti.de>
2026-02-18 20:39:36 +0100

    chore: set location for backup dirs

--- sync.bat
+++ sync.bat
@@ -8,10 +8,12 @@ REM -- Hier deine Pfade eintragen: --
 set "PATHA=C:\Test"
 set "PATHB=C:\Users\user\scripts\Test"
 
+REM -- Ins Script-Verzeichnis wechseln (fuer Log/Backup-Ablage) --
+cd /d "%~dp0"
+
 REM -- Script ausfuehren (ohne Execution Policy) --
 powershell.exe -NoProfile -Command "& { $s = Get-Content -Path '%~dp0sync.ps1' -Raw; $sb = [scriptblock]::Create($s); & $sb -PathA '%PATHA%' -PathB '%PATHB%' }"
 
 echo.
 echo Fertig! (Exit Code: %ERRORLEVEL%)
 pause
-
